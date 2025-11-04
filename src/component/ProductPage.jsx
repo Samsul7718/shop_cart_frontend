8ae6a40f05506ba8740e344ca0638f706b3d0dee
samsul alam <samsul7718@gmail.com>
2025-11-04 11:46:19 +0530

    new url added in frontend

--- src/component/ProductPage.jsx
+++ src/component/ProductPage.jsx
@@ -11,7 +11,7 @@ const ProductPage = () => {
   const navigate=useNavigate()
 
     useEffect(()=>{
-    fetch("http://localhost:3000/api/product")
+    fetch("https://shop-cart-backend-ten.vercel.app/api/product")
     .then(res=>res.json())
     .then(data=>setProducts(data))
     .catch(err=>console.log(err))
